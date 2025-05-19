f71f9336e4fc37d55582eef4c6e4eefd1e652eec
mohamedahmed66972007 <123965732+mohamedahmed66972007@users.noreply.github.com>
2025-05-19 04:46:56 +0300

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -61,7 +61,8 @@ const setupSession = (app: Express) => {
         secure: process.env.NODE_ENV === "production",
         maxAge: 24 * 60 * 60 * 1000, // 24 hours
         httpOnly: true,
-        path: '/'
+        path: '/',
+        sameSite: 'none'
       },
     })
   );
